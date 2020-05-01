0b99622b72e562314a65436b9536355e58e777a5
shkhan922 <shkhan922@gmail.com>
2020-05-01 05:55:55 +0530

    updated code

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\silico\downloads\telegram desktop\rappo\rappo\themes\rappo\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
